d8e777f9887c813ab2ba8fb24a09a408eddf8678
john Maxwell <iuiuiepi@outlook.com>
2019-04-27 22:29:04 +0800

    完成了home page (重复的部分先跳过了= =)

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,15 @@
 import './normalize.css'
 import './index.css'
+window.onload = dotFollow;
+function dotFollow() {
+  let myDiv: HTMLDivElement = <HTMLDivElement>document.querySelector('.dot');
+  window.onmousemove = (ev) => {
+    // let ofLeft = document.body.offsetLeft;
+    // let ofTop = document.body.offsetTop;
+    let oTop =ev.clientY;
+    let oLeft = ev.clientX;
+    myDiv.style.display = "block";
+    myDiv.style.left = oLeft.toString()+"px";
+    myDiv.style.top = oTop.toString()+"px";
+  }
+}
